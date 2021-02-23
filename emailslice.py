bfcc385be66fc96ca4361debbc29beb5de35d739
NIKHIL ISAAC <78369872+NikhiIsaac@users.noreply.github.com>
2021-02-23 22:12:53 +0530

    Create emailslice.py

---
+++ emailslice.py
@@ -0,0 +1,16 @@
+popular_domains = {
+"gmail": "Google",
+"hotmail":"Microsoft",
+"yahoo":"Yahoo",
+"outlook":"Microsoft"}
+
+user_email = input("What is your email address? ")
+
+user_name = user_email.split('@')[0]
+user_domain = user_email.split('@')[-1]
+user_domain = user_domain.split('.')[0]
+
+if user_domain in popular_domains.keys():
+print(f"Hey {user_name} it seems like your email is registered with {popular_domains[user_domain]}!")
+else:
+print(f"Hey {user_name} it seems like you have your own custom domain at {user_domain}!")
